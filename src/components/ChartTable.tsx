77b1138f59a56c5f3adf3ca1fac475787e50f784
이솜이 <122191856+somyiLee@users.noreply.github.com>
2023-09-12 23:30:28 +0900

    Feat: filter, highlight 로직 추가 - recoil install :  atom 추가, chartData 전역관리 - filter button 추가 - useChrtFilter 훅 추가

--- src/components/ChartTable.tsx
+++ src/components/ChartTable.tsx
@@ -1,19 +1,21 @@
-/* eslint-disable react-refresh/only-export-components */
 import { Chart } from 'react-chartjs-2';
 import { ChartData, ChartOptions } from 'chart.js';
-import { forwardRef } from 'react';
-import { ChartItem } from '@/types';
+import { MouseEventHandler, forwardRef } from 'react';
+import { ChartDataType } from '@/types';
 
 interface TimeSeriesChartProps {
-  chartData: ChartData<'bar' | 'line', ChartItem[]>;
+  chartData: ChartData<'bar' | 'line', ChartDataType[]>;
   chartOption: ChartOptions<'bar' | 'line'>;
+  handleFilter: MouseEventHandler<HTMLCanvasElement>;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default forwardRef<any, TimeSeriesChartProps>(({ chartData, chartOption }, ref) => {
-  return (
-    <div>
-      <Chart type="bar" data={chartData} options={chartOption} ref={ref} />
-    </div>
-  );
-});
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, react-refresh/only-export-components
+export default forwardRef<any, TimeSeriesChartProps>(
+  ({ chartData, chartOption, handleFilter }, ref) => {
+    return (
+      <div>
+        <Chart type="bar" data={chartData} options={chartOption} ref={ref} onClick={handleFilter} />
+      </div>
+    );
+  },
+);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export * from './chartHelper';
-export * from './chartOption';
 export * from './registChartJS';
 export * from './date';
