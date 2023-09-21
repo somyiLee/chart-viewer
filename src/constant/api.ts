586e34832854574d7d829584f179713d293e1874
이솜이 <122191856+somyiLee@users.noreply.github.com>
2023-09-21 21:33:36 +0900

    Modify : add constant/api & chart files

---
+++ src/constant/api.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:5173/' : '';
+
+export const API_URL = '/data/chartList.json';
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,21 +1,2 @@
-export const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:5173/' : '';
-
-export const API_URL = '/data/chartList.json';
-
-export const AREA_KEY = 'value_area';
-export const BAR_KEY = 'value_bar';
-
-export const X_KEY = 'x';
-export const Y_KEY = 'y';
-
-export const COLORS = {
-  purple: { normal: '#9ea1fe', dark: '#5741c0' },
-  pink: { normal: 'rgba(242, 130, 149, 0.3)', dark: 'rgba(242, 130, 149, 0.9)' },
-} as const;
-
-export const FILTER_LIST = [
-  { id: 1, content: '강남구' },
-  { id: 2, content: '노원구' },
-  { id: 3, content: '성북구' },
-  { id: 4, content: '중랑구' },
-];
+export * from './api';
+export * from './chart';
